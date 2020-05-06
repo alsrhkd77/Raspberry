8f9cc84030d7db2f3ac731b37637433db57f2d37
MinGwang Song <43804413+alsrhkd77@users.noreply.github.com>
2020-05-07 01:03:41 +0900

    Update readuart.py

--- readuart.py
+++ readuart.py
@@ -3,4 +3,6 @@ import serial
 con = serial.Serial('/dev/ttyS0', 9600)  # 라즈베리파이 1, 2인 경우 /dev/ttyAMA0
 while True:
     text = input("Input any text message: ")
-con.write(text + '\r\n')
+    text = text + '\r\n'
+    text = bytes(text.encode("utf-8"))
+    con.write(text)
