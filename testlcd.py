f692f8c8546aac6aea73d3362e95e36c362507c6
MinGwang Song <43804413+alsrhkd77@users.noreply.github.com>
2020-05-07 01:53:35 +0900

    Create testlcd.py

---
+++ testlcd.py
@@ -0,0 +1,11 @@
+from Adafruit_CharLCD import Adafruit_CharLCD
+from time import sleep
+
+lcd = Adafruit_CharLCD(rs=22, en=11, d4=23, d5=10, d6=9, d7=25, cols=16, lines=2)
+i = 0
+
+while True:
+    lcd.clear()
+    lcd.message('Counting: ' + str(i))
+    sleep(1)
+    i = i + 1
