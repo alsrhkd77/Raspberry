ad7945504d7d71819e57fce8f318627da90e9912
MinGwang Song <43804413+alsrhkd77@users.noreply.github.com>
2020-05-07 00:28:26 +0900

    Update dht22db.py

--- dht22db.py
+++ dht22db.py
@@ -17,7 +17,8 @@ def get_value():
     if humidity is not None and temperature is not None:
         now = datetime.datetime.now()
         nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
-        cur.execute("insert into dht22(time, temp, hum) value(%s,%10.2f,%s)", (nowDatetime, temperature, humidity))
+        # cur.execute("insert into dht22(time, temp, hum) value(%s,%10.2f,%s)", (nowDatetime, temperature, hu\midity))
+        cur.execute(f'''INSERT INTO dht22 VALUES(\'{nowDatetime}\', \'{temperature}\', \'{humidity}\');''')
         db.commit()
 
 @app.route('/')
