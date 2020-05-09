3da97c9099912227fbcad066faf4e126d74d2a02
mingwang <alsrhkd77@gmail.com>
2020-05-09 20:28:47 +0900

    Update lirc.py

--- lirc.py
+++ lirc.py
@@ -1,18 +1,4 @@
-import RPi.GPIO as GPIO
-from lirc import LircdConnection
-import time
+import lirc
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(24, GPIO.OUT)
-ON = "on"
-OFF = "off"
-
-with LircdConnection("airconditionor") as conn:
-    while (True):
-        codeIR = conn.readline()
-        if len(codeIR) != 0:
-            print(codeIR)
-        if codeIR == ON:
-            GPIO.output(24, True)
-        elif codeIR == OFF:
-            GPIO.output(24, False)
+sockid = lirc.init("myprogram", "mylircrc")
+lirc.load_config_file("~/airconditionor.lircd.conf")
