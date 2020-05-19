7d02a2c81a90d2a8f9005b4aed7d712c1a51bb05
mingwang <alsrhkd77@gmail.com>
2020-05-19 18:21:11 +0900

    Create pir.py

---
+++ pir.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+​
+GPIO.setmode(GPIO.BCM)
+​
+pir = 24
+GPIO.setup(pir, GPIO.IN)
+​
+
+def loop():
+    cnt = 0
+    while True:
+        cur_stat = GPIO.input(pir)
+        if cur_stat == 1:
+            cnt += 1
+            print("%d번 감지됨" % cnt)
+
+
+try:
+    loop()
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
