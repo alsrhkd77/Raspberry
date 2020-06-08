302e5b67d253247314e56cea421884505e99a4a0
mingwang <alsrhkd77@gmail.com>
2020-06-08 22:24:59 +0900

    Update beaconscanner.py

--- beaconscanner.py
+++ beaconscanner.py
@@ -10,13 +10,14 @@ def measureDistance(txPower, rssi):
         return pow(ratio, 10)
     else:
         return (0.89976) * pow(ratio, 7.7095) + 0.111
+
+
 scan = pexpect.spawn("sudo hcitool lescan --duplicates 1>/dev/null")
 p = pexpect.spawn("sudo hcidump --raw")
 capturing = 0
 packet = ""
 while True:
     line = p.readline()
-    print(line)
     if not line: break
     if capturing == 0:
         if line[0] == '>':
@@ -26,14 +27,14 @@ while True:
         if re.match("^[0-9a-fA-F]{2}\[0-9a-fA-F]", line.strip()):
             packet += ' ' + line.strip()
         elif re.match("^04\3E\2A\02\01\.{26}\02\01\.{14}\02\15", packet):
-            #print(" packet = " + packet)
+            print(" packet = " + packet)
             UUID = packet[69:116].replace(' ', '')
             UUID = UUID[0:8] + '-' + UUID[8:12] + '-' + UUID[12:16] + '-' + UUID[16:20] + '-' + UUID[20:]
-            MAJOR=int(packet[117:122].replace(' ', ''), 16)
+            MAJOR = int(packet[117:122].replace(' ', ''), 16)
             MINOR = int(packet[123:128].replace(' ', ''), 16)
-            POWER=int(packet[129:131].replace(' ', ''),16)- 256
-            RSSI = int(packet[132:134].replace(' ', ''),16)- 256
-            if len (sys.argv) != 1 and sys.argv[1] == "-b" :
+            POWER = int(packet[129:131].replace(' ', ''), 16) - 256
+            RSSI = int(packet[132:134].replace(' ', ''), 16) - 256
+            if len(sys.argv) != 1 and sys.argv[1] == "-b":
                 print(UUID, MAJOR, MINOR, POWER, RSSI)
             else:
                 print("UUID: %s MAJOR: %d MINOR: %d POWER: %d RSSI: % d") % (UUID, MAJOR, MINOR, POWER, RSSI)
